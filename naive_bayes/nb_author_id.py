328c9598d93ba03029c79838ba38dd7c6c37823b
Alexandru Oancea <alx.oancea@gmail.com>
2019-04-26 13:55:27 +0300

    lesson-02: final project, fix non-backwards compatibility issues in sklearn

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,23 +11,47 @@
 """
     
 import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+import os
+import time
 
+# print os.getcwd()
+
+# os.chdir(path)
+# sys.path.append("E:\\projects\\machine-learning-tutorials\\ud120-projects\\tools")
+
+sys.path.append("../tools")
+
+# print sys.path
+# print sklearn.__version__
+
+from email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
+
+training_time = time.time()
+
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time.time()-training_time, 3), "s"
+
+prediction_time = time.time()
+
+pred = clf.predict(features_test)
+
+print "prediction time:", round(time.time()-prediction_time, 3), "s"
+
+accuracy = accuracy_score(labels_test, pred)
+
+print accuracy
 #########################################################
-
-
