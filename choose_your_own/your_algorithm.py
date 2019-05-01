21366a32dda2a01813390d2eb4adb6ca9cf568a0
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-01 11:16:02 +0300

    lesson-05: final project k-nn

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,12 +31,30 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from time import time
 
+# clf = KNeighborsClassifier(n_neighbors=1, p=2, metric='euclidean')
+clf = KNeighborsClassifier(n_neighbors=9, p=1, metric='minkowski')
 
+training_time = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-training_time, 3), "s"
 
+prediction_time = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-prediction_time, 3), "s"
 
+accuracy = accuracy_score(labels_test, pred)
+f1 = f1_score(labels_test, pred)
+cm = confusion_matrix(labels_test, pred)
 
-
+print accuracy
+print f1
+print cm
 
 try:
     prettyPicture(clf, features_test, labels_test)
