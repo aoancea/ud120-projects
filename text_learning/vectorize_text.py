a2f8da68a7fa54c47ef3157379b7c8f7d35f3f47
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-03 10:00:53 +0300

    lesson-11: final project text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
 
+
 """
     Starter code to process the emails from Sara and Chris to extract
     the features and get the documents ready for classification.
@@ -42,20 +43,35 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
+        if temp_counter > 0:
+            path = os.path.join('..\..', path[:-1])
+            # print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            parsed_email = parseOutText(email)
+            # print parsed_email
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
 
+            parsed_email = parsed_email.replace('sara', '').replace('shackleton', '').replace('chris', '').replace('germani', '')
+
+            # parsed_email = ' '.join([word for word in parsed_email.split(' ') if word != 'sara' and word != 'shackleton' and word != 'chris' and word != 'germani'])
+
+            # parsed_email = ' '.join([word for word in parsed_email.split(' ') if word != 'sara'])
+            # parsed_email = ' '.join([word for word in parsed_email.split(' ') if word != 'shackleton'])
+            # parsed_email = ' '.join([word for word in parsed_email.split(' ') if word != 'chris'])
+            # parsed_email = ' '.join([word for word in parsed_email.split(' ') if word != 'germani'])
+            # print parsed_email
+
             ### append the text to word_data
+            word_data.append(parsed_email)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -68,8 +84,13 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
-
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit(word_data)
+
+# print vectorizer.get_feature_names()
+print len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34597]
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -2,6 +2,8 @@
 
 from nltk.stem.snowball import SnowballStemmer
 import string
+from sklearn.feature_extraction import stop_words
+
 
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
@@ -28,15 +30,29 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
+        stemmer = SnowballStemmer('english')
         
+        # print text_string
+        # print text_string.split(' ')
 
+        # print text_string.replace('\n', ' ').replace('\t', ' ')
+        # print text_string.replace('\n', ' ').replace('\t', ' ').split(' ')
 
+        # text_string = [stemmer.stem(w.replace('\n', ' ').replace('\t', '')) for w in text_string.split(' ') if w != '']
 
+        text_string = [stemmer.stem(w) for w in text_string.replace('\n', ' ').replace('\t', ' ').split(' ') if w != '']
+        
+        # print text_string
+
+        # text_string = [w for w in text_string if w not in stop_words.ENGLISH_STOP_WORDS]
+
+        words = ' '.join(text_string)
+        # print words
 
     return words
 
