1d91c7991deb6a84ce148460f816d53c0cbd6846
Alexandru Oancea <alx.oancea@gmail.com>
2019-05-05 10:04:46 +0300

    lesson-15: final project evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,24 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+
+pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, pred)
+
+print accuracy
+
+print len(pred)
+print confusion_matrix(y_test, pred)
+print precision_score(y_test, pred)
+print recall_score(y_test, pred)
